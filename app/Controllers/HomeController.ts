68b233e52a22981986b5d47cbdb76499db83b042
Lucas Silva <dev.lucassilva@gmail.com>
2023-04-28 15:28:34 -0300

    listando artigos na home

--- app/Controllers/HomeController.ts
+++ app/Controllers/HomeController.ts
@@ -1,7 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import Article from 'App/Models/Article';
 export default class HomeController {
     public async index({ inertia }: HttpContextContract) {
-        return inertia.render('Home');
+        const lastArticles = await Article.query().select('*').limit(2)
+        const limitedRows = lastArticles.map((article) => {
+           article.content = article.content.substring(0,30);
+           return article;
+        });
+
+        return inertia.render('Home', {
+            articles: limitedRows
+        });
     }
 }
--- resources/views/pages/Home.vue
+++ resources/views/pages/Home.vue
@@ -1,12 +1,21 @@
+<script setup>
+defineProps({
+    articles: Object
+})
+</script>
 
 <template>
     <h1 class="tw-flex tw-w-full tw-text-slate-600 tw-font-bold tw-mb-4">Ultimos artigos</h1>
+
     <ul class="tw-grid tw-grid-cols-2 md:tw-grid-cols-4 tw-gap-4">
-        <li class="tw-bg-white tw-rounded-lg tw-overflow-hidden tw-shadow-md" v-for="n in 8">
+        <li class="tw-bg-white tw-rounded-lg tw-overflow-hidden tw-shadow-md" 
+        v-for="(article,key) in articles" :key="key">
             <img src="https://placehold.it/300x200" alt="Thumbnail" class="tw-w-full tw-h-48 tw-object-cover">
             <div class="tw-p-4">
-                <h3 class="tw-font-bold tw-text-lg tw-mb-2">Titulo do Artigo 1</h3>
-                <p class="tw-text-gray-700 tw-mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+                <h3 class="tw-font-bold tw-text-lg tw-mb-2">{{ article.title }}</h3>
+                <p class="tw-text-gray-700 tw-mb-2">
+                    {{ article.content }}
+                </p>
                 <a href="#" class="tw-block tw-bg-blue-500 tw-text-white tw-px-4 tw-py-2 
                 tw-rounded hover:tw-bg-blue-600 tw-text-center">Ler Mais</a>
             </div>
